8c8d3256c088c08ce7a208743df61b48772967c9
igor-a-vm <igor-a-vm@yandex.ru>
2024-10-23 22:22:02 +0300

    fix logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
-import { AppModule } from './app.module';
 
 @Injectable()
 export class AppService {
