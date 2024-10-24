be1bf9933c066a36362f90989a0658cd5a209976
igor-a-vm <igor-a-vm@yandex.ru>
2024-10-24 14:55:34 +0300

    autocommit: update docs and format

--- src/modules/purchase/purchase.controller.spec.ts
+++ src/modules/purchase/purchase.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PurchaseController } from './purchase.controller';
 
 describe('PurchaseController', () => {
-  let controller: PurchaseController;
+    let controller: PurchaseController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PurchaseController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [PurchaseController],
+        }).compile();
 
-    controller = module.get<PurchaseController>(PurchaseController);
-  });
+        controller = module.get<PurchaseController>(PurchaseController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
